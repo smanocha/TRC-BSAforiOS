7d0e10fc219e1b5d5ce6290bc8c96fe915638f6f
Sugam Manocha <SugamManocha@Sugams-MacBook-Pro.local>
2017-05-01 23:52:00 -0400

    no message

--- TRC-BSA/TRC-BSA/ViewController.swift
+++ TRC-BSA/TRC-BSA/ViewController.swift
@@ -22,6 +22,7 @@ class ViewController: UIViewController {
         buttonClicked = "jsb"
     }
     override func viewDidLoad() {
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
