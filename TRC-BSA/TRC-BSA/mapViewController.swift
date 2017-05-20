41fa07c0c350b3cae4483625ff78e96adfb6bdd5
Linda L. Li <linda.l.li@stonybrook.edu>
2017-05-20 01:08:24 -0400

    added all map features

--- TRC-BSA/TRC-BSA/mapViewController.swift
+++ TRC-BSA/TRC-BSA/mapViewController.swift
@@ -18,12 +18,23 @@ class mapViewController: UIViewController {
     let craftLodgePDF = "2017-OSR-Program-Guide-Craft-Lodge-Page"
     let councilHousePDF = "2017-OSR-Program-Guide-Communications-Page"
     
+    let bobCatShelterPDF = "2017 OSR Program Guide -  Nature Page"
+    let longHousePDF = "2017 OSR Program Guide - ATV Page"
+    let westShelterPDF = "2017 OSR Program Guide - Field Sports Page"
+    let councilHouseHealthSafetyPDF = "2017 OSR Program Guide - Health & Saftey Page"
+    let maintainenceBuildingPDF = "2017 OSR Program Guide - Older Boy Page"
+    let rappahannockPDF = "2017 OSR Program Guide - Paintball Page"
+    let tradingPostLongHousePDF = "2017 OSR Program Guide - Sales & Finance Page"
+    let otterShelterPDF = "2017 OSR Program Guide - Scout Craft Page"
+    let danBeardShelterRangesPDF = "2017 OSR Program Guide - Shooting Sports Page"
+    let councilHouseSTEMPDF = "2017 OSR Program Guide - STEM Page"
+    let trSheltherPDF = "2017 OSR Program Guide - The Glaize Page"
     
-    @IBOutlet weak var Oneonta: UIImageView!
+    @IBOutlet weak var Onteora: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.Oneonta.image = UIImage(named:"osrCampMap")
+        self.Onteora.image = UIImage(named:"osrCampMap")
         
         
         // Do any additional setup after loading the view.
@@ -73,6 +84,97 @@ class mapViewController: UIViewController {
             self.view.addSubview(webView)
         }
     }
+    
+    
+    @IBAction func openBobcatShelterPDF(_ sender: Any) {
+        if let url = Bundle.main.url(forResource: bobCatShelterPDF, withExtension: "pdf"){
+            let webView = UIWebView(frame: self.view.frame)
+            let urlRequest = URLRequest(url:url)
+            webView.loadRequest(urlRequest as URLRequest)
+            self.view.addSubview(webView)
+        }
+
+    }
+    
+    @IBAction func openLongHousePDF(_ sender: Any) {
+        if let url = Bundle.main.url(forResource: longHousePDF, withExtension: "pdf"){
+            let webView = UIWebView(frame: self.view.frame)
+            let urlRequest = URLRequest(url:url)
+            webView.loadRequest(urlRequest as URLRequest)
+            self.view.addSubview(webView)
+        }
+
+    }
+    
+    
+    @IBAction func openWestShelterPDF(_ sender: Any) {
+        if let url = Bundle.main.url(forResource: westShelterPDF, withExtension: "pdf"){
+            let webView = UIWebView(frame: self.view.frame)
+            let urlRequest = URLRequest(url:url)
+            webView.loadRequest(urlRequest as URLRequest)
+            self.view.addSubview(webView)
+        }
+
+    }
+    
+    @IBAction func openCouncilHouseHealthSafetyPDF(_ sender: Any) {
+        if let url = Bundle.main.url(forResource: councilHouseHealthSafetyPDF, withExtension: "pdf"){
+            let webView = UIWebView(frame: self.view.frame)
+            let urlRequest = URLRequest(url:url)
+            webView.loadRequest(urlRequest as URLRequest)
+            self.view.addSubview(webView)
+        }
+    }
+    
+    
+    @IBAction func openMaintainenceBuildingPDF(_ sender: Any) {
+        if let url = Bundle.main.url(forResource: maintainenceBuildingPDF, withExtension: "pdf"){
+            let webView = UIWebView(frame: self.view.frame)
+            let urlRequest = URLRequest(url:url)
+            webView.loadRequest(urlRequest as URLRequest)
+            self.view.addSubview(webView)
+        }
+    }
+    
+    @IBAction func openOtterShelterPDF(_ sender: Any) {
+        if let url = Bundle.main.url(forResource: otterShelterPDF, withExtension: "pdf"){
+            let webView = UIWebView(frame: self.view.frame)
+            let urlRequest = URLRequest(url:url)
+            webView.loadRequest(urlRequest as URLRequest)
+            self.view.addSubview(webView)
+        }
+
+    }
+    
+    
+    @IBAction func openDanBeardShelterRangesPDF(_ sender: Any) {
+        if let url = Bundle.main.url(forResource: danBeardShelterRangesPDF, withExtension: "pdf"){
+            let webView = UIWebView(frame: self.view.frame)
+            let urlRequest = URLRequest(url:url)
+            webView.loadRequest(urlRequest as URLRequest)
+            self.view.addSubview(webView)
+        }
+    }
+    
+    
+    @IBAction func openCouncilHouseSTEMPDF(_ sender: Any) {
+        if let url = Bundle.main.url(forResource: councilHouseHealthSafetyPDF, withExtension: "pdf"){
+            let webView = UIWebView(frame: self.view.frame)
+            let urlRequest = URLRequest(url:url)
+            webView.loadRequest(urlRequest as URLRequest)
+            self.view.addSubview(webView)
+        }
+    }
+    @IBAction func openTRSheltherPDF(_ sender: Any) {
+        if let url = Bundle.main.url(forResource: trSheltherPDF, withExtension: "pdf"){
+            let webView = UIWebView(frame: self.view.frame)
+            let urlRequest = URLRequest(url:url)
+            webView.loadRequest(urlRequest as URLRequest)
+            self.view.addSubview(webView)
+        }
+
+    }
+    
     
 }
 
